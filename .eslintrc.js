ec549315d316c1fa7a3260ac257b6f11d99bcb42
Hiroiku Inoue <work@hiroiku.me>
2024-04-11 13:48:08 +0900

    ✨ 新機能: ESLintとPrettierの設定を追加し、コードの品質を向上 ✨ 新機能: .gitignoreを追加し、不要なファイルをGit追跡から除外 ✨ 新機能: README.mdを追加し、プロジェクトの説明を提供 ✨ 新機能: package.jsonを追加し、依存関係とスクリプトを定義 ✨ 新機能: ハッシュ生成機能を持つmain.tsを追加 ✨ 新機能: TypeScriptのコンパイラオプションを定義するtsconfig.jsonを追加

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'import/order': ['error'],
+    'import/no-unresolved': 'off',
+    'max-len': [2, 120, 2, { ignoreStrings: true }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { createHash } from 'crypto';
+
+async function calculateFileHash(url: string): Promise<string> {
+  const hash = createHash('sha256');
+  const response = await fetch(url);
+  const reader = response.body?.getReader();
+
+  if (!reader) {
+    throw new Error('Failed to get reader');
+  }
+
+  let done, value;
+  do {
+    ({ done, value } = await reader.read());
+
+    if (value) {
+      hash.update(new Uint8Array(value));
+    }
+  } while (!done);
+
+  return hash.digest('hex');
+}
+
+(async () => {
+  const url = 'https://www.soumu.go.jp/main_content/000487281.mp4';
+  const hash = await calculateFileHash(url);
+  console.log(`Hash: ${hash}`);
+})();
